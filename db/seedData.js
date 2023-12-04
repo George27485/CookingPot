c5ccc077472e7e69b8c2e2192a1a171becfdddc1
George Alvarez <georgealvarez@georges-mbp.mynetworksettings.com>
2023-12-03 22:31:13 -0500

    updated tables

--- db/seedData.js
+++ db/seedData.js
@@ -10,7 +10,20 @@ async function createTables() {
                 "category" TEXT,
                 "dishImg" TEXT
             );
-            CREATE TABLE users(
+
+            CREATE TABLE ingredients (
+                "ingredientId" SERIAL PRIMARY KEY,
+                "name" VARCHAR(255) NOT NULL
+            );
+
+            CREATE TABLE recipe_ingredients (
+                "recipeId" INTEGER REFERENCES recipes("recipeId"),
+                "ingredientId" INTEGER REFERENCES ingredients("ingredientId"),
+                "quantity" VARCHAR(100),
+                PRIMARY KEY ("recipeId", "ingredientId")
+            );
+
+            CREATE TABLE users (
                 "userId" SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
@@ -18,20 +31,19 @@ async function createTables() {
                 is_admin BOOLEAN DEFAULT false NOT NULL
             );
 
-
-            CREATE TABLE comments(
+            CREATE TABLE comments (
                 "commentId" SERIAL PRIMARY KEY,
                 text VARCHAR(255) NOT NULL,
                 username VARCHAR(255) NOT NULL,
                 "userId" INTEGER REFERENCES users("userId"),
-                "recipeId" INTEGER REFERENCES recipes ("recipeId")
+                "recipeId" INTEGER REFERENCES recipes("recipeId")
             );  
-
         `);
-        console.log("Table 'recipes' created successfully.");
+        console.log("Tables created successfully.");
     } catch (error) {
-        console.error("Error creating table:", error);
+        console.error("Error creating tables:", error);
     }
 }
 
-createTables();  
+createTables();
+
